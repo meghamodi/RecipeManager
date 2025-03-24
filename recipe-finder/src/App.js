99fb19722c0b54cf8061dc450c468d30166041e1
Megha Modi <meghamodi007@gmail.com>
2025-03-24 15:17:48 -0700

    Re-added package.json and package-lock.json after resolving stash conflicts

---
+++ recipe-finder/src/App.js
@@ -0,0 +1,94 @@
+import './App.css';
+import { SearchRecipe } from './RecipeCard/searchRecipe.js';
+import RandomRecipes from './RandomRecipes.js'
+import {useState} from 'react';
+
+function App() {
+    const [isActive, setIsActive] = useState('homeText')
+
+  const handleContent = (contentId) =>{
+            setIsActive(contentId)   
+    }
+
+  return (
+    <div className="App">
+        <div className="container">
+            <div className="headers">
+                <div className="category">
+                    <h3  onClick={() =>handleContent('homeText')}>Home</h3> 
+                </div>
+            
+            <div className="category">
+                
+                <h3 onClick={()=> handleContent('quickRecipes')} >On the Go Recipes</h3>
+                    
+            </div>
+
+            <div className="category">
+                <h3  onClick={()=> handleContent('glowRecipes')}>GlowRecipes</h3>
+      
+            </div>
+
+            <div className="category">
+                <h3 onClick={()=> handleContent('AboutMe')}>About</h3>
+      
+            </div>
+            <div className="category">
+                <h3 onClick={()=> handleContent('RandomRecipe')}>Random Recipe</h3>
+      
+            </div>
+            
+        </div>
+        </div>
+
+        <div className="content-grid">
+            {/* <!-- Home Content --> */}
+            {isActive === 'homeText' && (
+            <div  className="content">
+                <p>This is home</p> 
+            </div> )}
+
+
+            {/* <!-- Quick Recipes Content --> */}
+            {/* It only executes the second part if the first part is true.
+ */}
+            {isActive === 'quickRecipes' && (
+            <div className="content">
+                <div>
+                    <SearchRecipe/>
+                    
+                   
+                </div>
+            </div>
+            )}
+
+            {/* <!-- Glow Content --> */}
+            {isActive === 'glowRecipes' && (
+            <div className="content">
+                <div>
+                    <p>Glow recipes coming soon</p>
+                </div>
+            </div>
+            )}
+
+            {/* <!-- About Content --> */}
+            {isActive === 'AboutMe' && (
+            <div className="content">
+                <p>This is the "About" section where you can add information about yourself or your website.</p>
+            </div>
+            )}
+
+            </div>
+            {/* <!-- About Content --> */}
+            {isActive === 'RandomRecipe' && (
+            <div className="content">
+                <RandomRecipes/>
+            </div>
+            )}
+      
+    </div>
+    
+  );
+}
+
+export default App;
